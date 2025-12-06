b4c6c4bfb8276948595a7edc0d98d2fa1772a08a
atakanakarsu <atakanakarsu@gmail.com>
2025-12-06 03:15:30 +0300

    Add react-dropzone dependency and type declarations

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,3 +2,23 @@
 declare module 'file-saver' {
     export function saveAs(data: Blob | File, filename?: string, options?: object): void;
 }
+
+declare module 'react-dropzone' {
+    export interface DropzoneOptions {
+        onDrop?: (acceptedFiles: File[], fileRejections: any[], event: any) => void;
+        accept?: Record<string, string[]>;
+        multiple?: boolean;
+        maxSize?: number;
+        maxFiles?: number;
+        disabled?: boolean;
+    }
+
+    export interface DropzoneState {
+        getRootProps: (props?: any) => any;
+        getInputProps: (props?: any) => any;
+        isDragActive: boolean;
+        acceptedFiles: File[];
+    }
+
+    export function useDropzone(options?: DropzoneOptions): DropzoneState;
+}
